9d718b21e80b47ea363c69f80c575f67f7e66438
poste02 <leprogrammeur@protonmail.com>
2023-12-05 11:24:45 -0800

    1ere maj du menu

--- app/src/main/java/com/example/winlabo/Accueil.java
+++ app/src/main/java/com/example/winlabo/Accueil.java
@@ -1,13 +1,48 @@
 package com.example.winlabo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Accueil extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.profile){
+            Toast.makeText(this, "Profile sélectionné", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.deconnexion) {
+            Toast.makeText(this, "Deconnexion sélectionné", Toast.LENGTH_SHORT).show();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        switch (item.getItemId()){
+//            case R.id.profile:
+//                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/winlabo/Enregistrement1.java
+++ app/src/main/java/com/example/winlabo/Enregistrement1.java
@@ -1,13 +1,38 @@
 package com.example.winlabo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Enregistrement1 extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.profile){
+            Toast.makeText(this, "Profile sélectionné", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.deconnexion) {
+            Toast.makeText(this, "Deconnexion sélectionné", Toast.LENGTH_SHORT).show();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
